f4805953e9d0a2d3f3004d7587a164141fe7abff
Ruturaj Apte <ruturaj.iitb@gmail.com>
2020-09-07 15:39:36 -0700

    fix missing import

--- api/app/models.py
+++ api/app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
-from app import db
+from app import db, login
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
